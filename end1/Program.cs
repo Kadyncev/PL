a4dfeaf398b65ad6a14fbd152b5821c77dee60f1
kadyncev <kadyncev@list.ru>
2023-01-30 19:13:45 +0500

    Задача 64

---
+++ end1/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Clear(); 
+
+Write("Введи N: ");
+int N = Convert.ToInt16(ReadLine());
+Write("Введи M: ");
+int M = Convert.ToInt16(ReadLine());
+WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Write($" {i}");
